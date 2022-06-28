e9b98767e92c0dc4e768f8526ae9de901e8c1d39
berna <bernabaran@gmail.com>
2022-06-27 23:10:15 -0400

    Day1

--- src/day1Berna.java
+++ src/day1Berna.java
@@ -1,2 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class day1Berna {
+    //Bir kelimenin icindeki harflerin kac adet oldugunu hesaplayan bir method yazin
+
+
+
+    public static void getCharcount(String name) {
+        Map<Character, Integer> charMap = new HashMap<Character, Integer>();
+        char strArray[] = name.toCharArray();
+        for (char c : strArray) {
+            if (charMap.containsKey(c)) {
+                charMap.put(c, charMap.get(c) + 1);
+            } else {
+                charMap.put(c, 1);
+
+            }
+        }
+        System.out.println(name + ":" + charMap);
+    }
+
+
+    public static void main(String[] args) {
+        getCharcount("helloworld");
+
+    }
 }
