ee1a886541359e3ad4ab7087fee95d2369461b8a
ezrac0des <ezraapaydin@gmail.com>
2022-07-06 22:43:23 -0400

    name

--- src/Day09_SumInteger.java
+++ src/Day09_SumInteger.java
@@ -1,16 +1,17 @@
+
+//SEVINC
 public class Day09_SumInteger {
 
     // Type code to find the sum of the integers from 12 to 15
     // 12 ile 15 arasındaki tam sayıların toplamını bulunuz
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         int sum = 0;
-        for(int i=12; i<15; i++) {
+        for (int i = 12; i < 15; i++) {
             sum = sum + i;
         }
         System.out.println(sum);
     }
-        }
-
+}
